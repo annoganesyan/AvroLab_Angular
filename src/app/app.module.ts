7c13b8effd80e06a6e998396f26bdab90ced7a5c
anna_ogan <oganesyanannag@gmail.com>
2020-10-29 09:01:40 -0700

    first commit - header+intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.compoent';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,

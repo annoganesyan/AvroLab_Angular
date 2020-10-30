acb094d6d51201f4d544cce51ad3414cf5d03923
anna_ogan <oganesyanannag@gmail.com>
2020-10-30 13:02:27 -0700

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.compoent';
 import { IntroComponent } from './intro/intro.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ServicesComponent } from './services/services.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { CareersComponent } from './careers/careers.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContanctComponent } from './contanct/contanct.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IntroComponent
+    IntroComponent,
+    PartnersComponent,
+    ServicesComponent,
+    TechnologiesComponent,
+    CareersComponent,
+    CoursesComponent,
+    ContanctComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
